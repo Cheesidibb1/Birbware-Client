576af749c822287cc74b4024cb5174879822bda2
Cheesidibb1 <ztech.z@icloud.com>
2024-11-27 16:35:35 -0700

    another update

--- Birbware Client/Birbware Client.cpp
+++ Birbware Client/Birbware Client.cpp
@@ -1,15 +1,81 @@
 #include <windows.h>
 #include <string>
-#include "resource.h"  // Include the resource header for icon identifier
+#include <gdiplus.h>   // GDI+ for loading PNG images
+#include "resource.h"  // Include the resource header for identifiers
+#pragma comment(lib, "gdiplus.lib")  // Link to the GDI+ library
+
+using namespace Gdiplus;
 
 // Window procedure function to handle events
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+    static HWND hButton1, hButton2, hButton3;  // Buttons for three icons
+    static ULONG_PTR gdiplusToken;
+
     switch (uMsg)
     {
+    case WM_CREATE:
+    {
+        // Initialize GDI+ library
+        GdiplusStartupInput gdiPlusStartupInput;
+        GdiplusStartup(&gdiplusToken, &gdiPlusStartupInput, NULL);
+
+        // Create buttons for icons at the bottom
+        hButton1 = CreateWindow(L"BUTTON", L"", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_ICON,
+            50, 600, 100, 100, hwnd, (HMENU)1, GetModuleHandle(NULL), NULL);
+        hButton2 = CreateWindow(L"BUTTON", L"", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_ICON,
+            200, 600, 100, 100, hwnd, (HMENU)2, GetModuleHandle(NULL), NULL);
+        hButton3 = CreateWindow(L"BUTTON", L"", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_ICON,
+            350, 600, 100, 100, hwnd, (HMENU)3, GetModuleHandle(NULL), NULL);
+
+        // Load PNG images using GDI+ from resources
+        HBITMAP hServerBitmap = NULL, hWrenchBitmap = NULL, hUserBitmap = NULL;
+
+        // Load the PNG resources using GDI+
+        Bitmap* pServerBitmap = Bitmap::FromResource(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_SERVER));
+        if (pServerBitmap && pServerBitmap->GetLastStatus() == Ok) {
+            pServerBitmap->GetHBITMAP(Color(255, 255, 255), &hServerBitmap);
+            SendMessage(hButton1, BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hServerBitmap);
+        }
+
+        Bitmap* pWrenchBitmap = Bitmap::FromResource(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_WRENCH));
+        if (pWrenchBitmap && pWrenchBitmap->GetLastStatus() == Ok) {
+            pWrenchBitmap->GetHBITMAP(Color(255, 255, 255), &hWrenchBitmap);
+            SendMessage(hButton2, BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hWrenchBitmap);
+        }
+
+        Bitmap* pUserBitmap = Bitmap::FromResource(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_USER));
+        if (pUserBitmap && pUserBitmap->GetLastStatus() == Ok) {
+            pUserBitmap->GetHBITMAP(Color(255, 255, 255), &hUserBitmap);
+            SendMessage(hButton3, BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hUserBitmap);
+        }
+
+        // Clean up
+        delete pServerBitmap;
+        delete pWrenchBitmap;
+        delete pUserBitmap;
+    }
+    break;
+
+    case WM_COMMAND:
+        // Handle button click events
+        if (LOWORD(wParam) == 1) {
+            MessageBox(hwnd, L"Server Icon Button clicked", L"Info", MB_OK);
+        }
+        else if (LOWORD(wParam) == 2) {
+            MessageBox(hwnd, L"Wrench Icon Button clicked", L"Info", MB_OK);
+        }
+        else if (LOWORD(wParam) == 3) {
+            MessageBox(hwnd, L"User Icon Button clicked", L"Info", MB_OK);
+        }
+        break;
+
     case WM_DESTROY:
+        // Shutdown GDI+ when window is destroyed
+        GdiplusShutdown(gdiplusToken);
         PostQuitMessage(0);
         return 0;
+
     case WM_PAINT:
     {
         PAINTSTRUCT ps;
@@ -21,9 +87,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         return 0;
     }
     }
+
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
+// Correct WinMain signature
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     // Step 1: Initialize the window class
@@ -38,13 +106,13 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
         return 0;
     }
 
-    // Step 2: Create the window
+    // Step 2: Create the window with WS_OVERLAPPED style (no resizing)
     HWND hwnd = CreateWindowEx(
         0,
         className,
         L"My Application",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 700, 650,
+        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_VISIBLE,  // No resize style here
+        CW_USEDEFAULT, CW_USEDEFAULT, 950, 750,
         NULL,
         NULL,
         hInstance,
@@ -56,7 +124,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
         return 0;
     }
 
-    // Step 3: Load the icon from the resources
+    // Step 3: Load the main window icon from the resources
     HICON hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));  // Load the icon using the identifier defined in resource.h
     if (hIcon) {
         SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);  // Set the big icon
--- Birbware Client/Resource.h
+++ Birbware Client/Resource.h
@@ -11,6 +11,12 @@
 #define IDC_BIRBWARECLIENT              109
 #define IDR_MAINFRAME                   128
 #define IDI_ICON1                       131
+#define IDB_SERVER                      132
+#define IDB_WRENCH                      133
+#define IDB_USER                        134
+#define IDB_PNG1                        138
+#define IDB_PNG2                        139
+#define IDB_PNG3                        140
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -18,7 +24,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_RESOURCE_VALUE        141
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
